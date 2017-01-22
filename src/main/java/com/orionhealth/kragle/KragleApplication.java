dad03a24c389a15a5d80cda37468f95b46733b86
Steven Wang <steven.wang@orionhealth.com>
2017-01-22 23:07:26 +1300

    skeleton

---
+++ src/main/java/com/orionhealth/kragle/KragleApplication.java
@@ -0,0 +1,12 @@
+package com.orionhealth.kragle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KragleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KragleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/orionhealth/kragle/KragleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orionhealth.kragle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KragleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
