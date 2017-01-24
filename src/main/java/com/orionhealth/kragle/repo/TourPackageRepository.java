b704107d902cfc6df3ef14179e844d59d0440ee9
Steven Wang <steven.wang@orionhealth.com>
2017-01-25 00:29:10 +1300

    Fix the resource issue and add param annotation

--- src/main/java/com/orionhealth/kragle/repo/TourPackageRepository.java
+++ src/main/java/com/orionhealth/kragle/repo/TourPackageRepository.java
@@ -2,10 +2,11 @@ package com.orionhealth.kragle.repo;
 
 import com.orionhealth.kragle.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by stevenw on 1/23/17.
  */
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    TourPackage findByName(String name);
+    TourPackage findByName(@Param("name") String name);
 }
--- src/main/java/com/orionhealth/kragle/repo/TourRepository.java
+++ src/main/java/com/orionhealth/kragle/repo/TourRepository.java
@@ -1,13 +1,16 @@
 package com.orionhealth.kragle.repo;
 
 import com.orionhealth.kragle.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 /**
  * Created by stevenw on 1/23/17.
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
